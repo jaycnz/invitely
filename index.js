7cf4f7c5e54a0c6cd8eba52ff1839f06857b274c
jaynz <ijaygt@gmail.com>
2026-09-17 14:28:20 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
